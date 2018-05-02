6b25515dd364427932f81e147560a7de5d50d637
PC-FABI\Fabian Hensel <fabianhensel6@googlemail.com>
2018-05-02 16:02:21 +0200

    Grundgerüst

---
+++ einheitenumrechner/server.js
@@ -0,0 +1,27 @@
+// Initialisierung von Sqlite3
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('shop.db', (error) => {
+	if (error) {
+		console.log(error.message);
+	}
+	else {
+		console.log('connected to the database');
+	}
+});
+
+// Initialisierung Express
+const express = require ('express');
+const app = express();
+
+// Initialisierung Body-Parser
+const bodyParser = require ('body-parser');
+app.use(bodyParser.urlencoded({extended: true});
+
+// Initialisierung Embedded Javascript (ejs)
+app.engine('.ejs', require('ejs').__express);
+app.set('view engine', 'ejs');
+
+const port = 3000;
+app.listen(port, function() {
+	console.log('listening to port ' + port);
+});
