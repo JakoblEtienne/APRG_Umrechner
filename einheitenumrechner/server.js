52c3ace9be0e4e0ce7b0a742d3d890cd07cb1d21
DESKTOP-MNCUD99\fabia <fabianhensel6@googlemail.com>
2018-05-03 14:31:17 +0200

    Grundgerüste der ejs-Seiten Zeit, Temperatur, Längen erstellt

--- einheitenumrechner/server.js
+++ einheitenumrechner/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Initialisierung Body-Parser
 const bodyParser = require ('body-parser');
-app.use(bodyParser.urlencoded({extended: true});
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Initialisierung Embedded Javascript (ejs)
 app.engine('.ejs', require('ejs').__express);
@@ -24,4 +24,8 @@ app.set('view engine', 'ejs');
 const port = 3000;
 app.listen(port, function() {
 	console.log('listening to port ' + port);
+});
+
+app.get('/laengen', (req, res) => {
+	res.render('laengen')
 });
